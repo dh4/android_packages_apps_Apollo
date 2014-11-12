f8c47516811b708a7d0efa9a26b18ac1c046d7af
Dan Hasting <dan@hasting.info>
2014-11-11 20:10:01 -0500

    Change app name to com.dh4.apollo

--- src/com/devspark/appmsg/AppMsg.java
+++ src/com/devspark/appmsg/AppMsg.java
@@ -25,7 +25,7 @@ import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import com.andrew.apollo.R;
+import com.dh4.apollo.R;
 
 /**
  * In-layout notifications. Based on {@link android.widget.Toast} notifications 
